3f96cedc4353bb4ce702cb82342a69b9e9a0fa41
ptrelford <ptrelford@localhost>
2013-01-27 10:56:18 +0000

    Extension methods for using specific Moq generic method overloads from F#

---
+++ Moq.FSharp.Extensions/Moq.FSharp.Extensions.fs
@@ -0,0 +1,17 @@
+﻿module Moq.FSharp.Extensions
+
+open System
+open System.Linq.Expressions
+
+// Moq extensions for F#
+type Moq.Mock<'TAbstract> when 'TAbstract : not struct with
+    /// Specifies a setup on the mocked type for a call to a function
+    member mock.SetupFunc<'TResult>(expression:Expression<Func<'TAbstract,'TResult>>) =
+        mock.Setup<'TResult>(expression)
+    /// Specifies a setup on the mocked type for a call to a void method
+    member mock.SetupAction(expression:Expression<Action<'TAbstract>>) = 
+        mock.Setup(expression)
+    /// Specifies a setup on the mocked type for a call to a property setter
+    member mock.SetupSetAction<'TProperty>(setupExpression:Action<'TAbstract>) 
+        : Moq.Language.Flow.ISetupSetter<'TAbstract,'TProperty> = 
+        mock.SetupSet<'TProperty>(setupExpression)
---
+++ Moq.FSharp.Extensions/Moq.FSharp.Usage.fsx
@@ -0,0 +1,38 @@
+﻿#r @"..\packages\Moq.4.0.10827\lib\NET40\Moq.dll"
+#load "Moq.FSharp.Extensions.fs"
+
+open System
+open Moq
+open Moq.FSharp.Extensions
+      
+let inline Assert success = if not success then failwith "Failed"
+
+type IFoo =
+    abstract DoSomething : string -> bool
+    abstract Value : int with get, set
+
+let ``mock a method that returns a value`` =
+    let mock = Mock<IFoo>()
+    mock.Setup<bool>(fun foo -> foo.DoSomething("ping")).Returns(true) |> ignore
+    Assert(mock.Object.DoSomething("ping"))
+    
+let ``mock a method that returns a value without using a type annotation`` =
+    let mock = Mock<IFoo>()
+    mock.SetupFunc(fun foo -> foo.DoSomething("ping")).Returns(true) |> ignore
+    Assert(mock.Object.DoSomething("ping"))  
+    
+let ``mock a property setter`` =
+    let mock = Mock<IFoo>()
+    let value = ref None
+    mock.SetupSet<int>(fun x -> x.Value <- It.IsAny<_>()).Callback(fun x -> value := Some x)
+    |> ignore
+    mock.Object.Value <- 1
+    Assert(!value |> Option.exists((=) 1))
+
+let ``mock a property setter without using a type annotation`` =
+    let mock = Mock<IFoo>()
+    let value = ref None
+    mock.SetupSetAction(fun x -> x.Value <- It.IsAny<_>()).Callback(fun x -> value := Some x)
+    |> ignore
+    mock.Object.Value <- 1
+    Assert(!value |> Option.exists((=) 1))
